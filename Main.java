3926e728f8724e7166dc75c8cd849e051fca2ca6
Renan Ricardo <77342102+rr23bk10@users.noreply.github.com>
2026-07-16 22:21:49 +0000

    ok

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
+
+        // Criação do primeiro personagem
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
 
-        .
-        .
-        .
+        // Criação do segundo personagem
+        Personagem heroi2 = new Personagem("Lyra", "Arqueira", 3, 85, 14.2);
 
+        // Exibe as informações dos personagens
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,29 @@
 public class Personagem {
+
+    // Atributos do personagem
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
+    // Construtor da classe
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
+    // Método para exibir as informações do personagem
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
+        System.out.println();
     }
 }
-
